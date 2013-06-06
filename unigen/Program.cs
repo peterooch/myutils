39083748f1fcbb921a32e8b19febff513d99fb50
Baruch Rutman <peterooch@gmail.com>
2013-06-06 11:09:24 +0300

    First progs
    getuname_test.cpp - getuname dll linking example
    unigen - convert the file
    http://www.unicode.org/Public/UNIDATA/UnicodeData.txt to a rc file
    stringtable

---
+++ unigen/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace unigen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Input File:");
+            string inf = Console.ReadLine();
+            Console.WriteLine("Output File:");
+            string outf = Console.ReadLine(); */
+            process();
+        }
+        static void process()
+        {
+            StreamReader input = new StreamReader("UnicodeData.txt");
+            ushort startdef = 0x0019;
+            ushort enddef = 0xFFFD;
+            StringBuilder rcfile = new StringBuilder();
+            string line;
+            TextInfo casefix = new CultureInfo("en-US", false).TextInfo;
+            while ((line = input.ReadLine()) != null)
+            {
+                StringBuilder templine = new StringBuilder();
+                string[] items = line.Split(';');
+                if (items[0] == "10000")
+                    break;
+                ushort ucode = ushort.Parse(items[0], System.Globalization.NumberStyles.HexNumber);
+                if (ucode > startdef && ucode < enddef)
+                    
+                    templine.Append(ucode.ToString() + " \"" + items[1] + "\" \n");
+                    rcfile.Append(casefix.ToTitleCase(templine.ToString().ToLower()));
+                    if (ucode == enddef)
+                        break;
+                
+                
+            }
+            using (StreamWriter output = new StreamWriter("res.txt"))
+            {
+                output.Write(rcfile.ToString());
+            }
+        }
+
+
+            
+
+
+        }
+    }
+
---
+++ unigen/getuname_test.cpp
@@ -0,0 +1,30 @@
+#include <Windows.h>
+#include <string.h>
+#include <stdio.h>
+int wmain()
+{
+    WCHAR desc[255];
+    int (FAR STDAPICALLTYPE * GetUName)(WORD wCharCode, LPWSTR lpbuf);
+    HINSTANCE hgetuname = NULL;
+    //getUName GetUName;
+    hgetuname = LoadLibraryW(L"getuname.dll");
+    if(hgetuname != NULL)
+    {
+        (FARPROC&)GetUName = /*(getUName)*/GetProcAddress(hgetuname,"GetUName");
+        if(GetUName == NULL)
+        {
+            FreeLibrary(hgetuname);
+            return 1;
+        }
+    }
+    WCHAR ch = getwchar();
+    int res = GetUName(ch,desc);
+    if(desc == NULL)
+    {
+        wprintf(L"Error %d",res);
+        return 1;
+    }
+    wprintf(L"U+%04X: %s",ch,desc);
+    return 0;
+}
+            
