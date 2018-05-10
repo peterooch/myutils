ecad008ad8d863bc88523340b78834b991b36154
Baruch Rutman <peterooch@gmail.com>
2018-05-10 19:17:00 +0300

    Dummy Program to test GCP and ETO
    Simple program that checks the results of GetCharacterPlacement and ExtTextOut function calls dealing with a 10 character "complex" string
    Based upon
    https://github.com/TransmissionZero/MinGW-Win32-Application

---
+++ rostestprogs/ETO_GCP_BIDI/dummy/MainWindow.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* Window procedure for our main window */
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+/* Register a class for our main window */
+BOOL RegisterMainWindowClass(void);
+
+/* Create an instance of our main window */
+HWND CreateMainWindow(void);
